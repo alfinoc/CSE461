b950c283c026c39f5a0d80e4aae378f47586d92e
alfinoc <chrisalfino@gmail.com>
2013-10-18 21:29:27 -0700

    a third edit

--- cse461/src/proj1/Protocol.java
+++ cse461/src/proj1/Protocol.java
@@ -4,5 +4,6 @@ public class Protocol {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("here's an edit.");
+		System.out.println("I wonder...");
 	}
 }
