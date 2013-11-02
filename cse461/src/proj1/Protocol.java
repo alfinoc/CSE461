c73a9e8949f708a31a3d7c5fc781f456d2d12480
alfinoc <chrisalfino@gmail.com>
2013-11-01 19:35:40 -0700

    the basics are in

--- cse461/src/proj1/Protocol.java
+++
@@ -1,9 +0,0 @@
-package proj1;
-
-public class Protocol {
-	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println("here's an edit.");
-		System.out.println("I wonder...");
-	}
-}
