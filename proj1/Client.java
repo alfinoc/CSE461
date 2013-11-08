83c7a0837250ff2484296de1b4aaabaddcc3a89b
Christopher Alfino <alfino27@attu2.cs.washington.edu>
2013-11-07 17:26:56 -0800

    working

--- proj1/Client.java
+++ proj1/Client.java
@@ -7,7 +7,6 @@ public class Client {
     public static final int SIZE_INT = 4;
 
     public static final String HOST = "bicycle.cs.washington.edu";
-    public static final int PORT = 12235;
 
     public static void main(String[] args) throws IOException {
 	// step a
@@ -21,20 +20,76 @@ public class Client {
 	byte[] resB = stepB(num, len, udpPort, secretA);
 	int tcpPort = PacketUtil.extractInt(resB, SIZE_HEADER);
 	int secretB = PacketUtil.extractInt(resB, SIZE_HEADER + SIZE_INT);
-	System.out.println("   tcp port: " + tcpPort);
-	System.out.println("   secret b: " + secretB);
+
+	// step c
+	Socket tcpSocket = new Socket(HOST, tcpPort);
+	byte[] resC = stepC(secretB, tcpSocket);
+	PacketUtil.printPacket(resC);
+	int num2 = PacketUtil.extractInt(resC, SIZE_HEADER);
+	int len2 = PacketUtil.extractInt(resC, SIZE_HEADER + SIZE_INT);
+	int secretC = PacketUtil.extractInt(resC, SIZE_HEADER + SIZE_INT * 2);
+	byte c = resC[SIZE_HEADER + SIZE_INT * 3];
+
+	// step d	
+	stepD(num2, len2, c, secretC, tcpSocket);
+    }
+
+    public static void stepD(int num, int len, byte c,
+			     int secretC, Socket socket) throws IOException {
+	System.out.println("num: " + num);
+	System.out.println("len: " + len);
+	System.out.println("c: " + c);
+
+	byte[] payload = new byte[len];
+	Arrays.fill(payload, c);
+
+	// send 'num' payloads
+	for (int i = 0; i < num; i++) {
+	    byte[] buf = PacketUtil.packetize(payload, PacketUtil.toByteArray(secretC), "d1");
+	    socket.getOutputStream().write(buf);
+	}
+
+	// read response
+	ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	InputStream in = socket.getInputStream();
+	byte[] buf = new byte[SIZE_HEADER + SIZE_INT];
+        int s;
+        int total = 0;
+        while (total < 4 && (s = in.read(buf)) != -1) {
+            total += s;
+            baos.write(buf, 0, s);
+        }
+	PacketUtil.printPacket(buf);
+	
+	socket.close();
+    }
+
+
+    public static byte[] stepC(int secretB, Socket socket) throws IOException {
+	InputStream in = socket.getInputStream();
+	ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+	byte[] buf = new byte[SIZE_HEADER + 4 * SIZE_INT];
+	int s;
+	int total = 0;
+	while (total < 16 && (s = in.read(buf)) != -1) {
+	    total += s;
+	    baos.write(buf, 0, s);
+	}
+
+	return baos.toByteArray();
     }
 
     // runs stepA, returning the received packet's data
     public static byte[] stepA() throws IOException {
 	System.out.println("Performing STEP A");
-	DatagramSocket socket = new DatagramSocket(5432);
+	DatagramSocket socket = new DatagramSocket();
 	socket.setSoTimeout(5000);
 	byte[] psecret0 = {0, 0, 0, 0};
 	byte[] bufOut = PacketUtil.packetize(PacketUtil.toByteArray("hello world"),
 					  psecret0, "a1");
 	InetAddress address = InetAddress.getByName(HOST);
-	DatagramPacket out = new DatagramPacket(bufOut, bufOut.length, address, PORT);
+	DatagramPacket out = new DatagramPacket(bufOut, bufOut.length, address, 12235);
 	socket.send(out);
 	byte[] bufIn = new byte[SIZE_HEADER + 4 * SIZE_INT];
 	DatagramPacket in = new DatagramPacket(bufIn, bufIn.length);
@@ -46,7 +101,7 @@ public class Client {
     public static byte[] stepB(int num, int len,
 			       int udpPort, int secretA) throws IOException {
 	System.out.println("Performing STEP B");
-	DatagramSocket socket = new DatagramSocket(udpPort);
+	DatagramSocket socket = new DatagramSocket();
 	socket.setSoTimeout(500);
 
 	InetAddress address = InetAddress.getByName(HOST);
