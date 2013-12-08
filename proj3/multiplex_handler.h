12d8c59685c4b40f160866c78a89a53bdcb46655
Ryan Bernstein <ryanbernstein1@gmail.com>
2013-12-07 17:50:23 -0800

    added handler header

---
+++ proj3/multiplex_handler.h
@@ -0,0 +1,17 @@
+#ifndef MULTIPLEX_HANDLER_HEADER
+#define MUTLIPLEX_HANDLER_HEADER
+
+
+struct handler_init {
+  // The file descriptor for a TCP connection to the client
+  int client_fd;
+  // The shared hash table for storing buffers
+  HashTable ht;
+};
+
+/*
+ * arg should be a (handler_init struct*)
+ */
+void *init(void* arg);
+
+#endif
